18ba20f84cb8b1da50cf74d8fccb38421ffa5999
zihua.zeng <zihua.zeng@shopee.com>
2023-08-04 17:48:15 +0800

    support single infer

--- scripts/machines/VX.py
+++ scripts/machines/VX.py
@@ -4,6 +4,7 @@ from progress.bar import Bar
 from tqdm import tqdm
 import pytorch_ssim
 import json
+import cv2
 import sys,time,os
 import torchvision
 from math import log10
@@ -18,6 +19,10 @@ from scripts.utils.losses import VGGLoss, l1_relative,is_dic
 from scripts.utils.imutils import im_to_numpy
 import skimage.io
 from skimage.measure import compare_psnr,compare_ssim
+
+from PIL import Image
+import torchvision.transforms as transforms
+from torchvision.utils import make_grid
 
 
 class Losses(nn.Module):
@@ -82,6 +87,71 @@ class Losses(nn.Module):
         att_loss =  self.attLoss(pred_ms, mask)
 
         return pixel_loss,att_loss,wm_loss,vgg_loss,ssim_loss
+
+
+def tensor2img(tensor, rgb2bgr=True, out_type=np.uint8, min_max=(0, 1)):
+    """Convert torch Tensors into image numpy arrays.
+
+    After clamping to [min, max], values will be normalized to [0, 1].
+
+    Args:
+        tensor (Tensor or list[Tensor]): Accept shapes:
+            1) 4D mini-batch Tensor of shape (B x 3/1 x H x W);
+            2) 3D Tensor of shape (3/1 x H x W);
+            3) 2D Tensor of shape (H x W).
+            Tensor channel should be in RGB order.
+        rgb2bgr (bool): Whether to change rgb to bgr.
+        out_type (numpy type): output types. If ``np.uint8``, transform outputs
+            to uint8 type with range [0, 255]; otherwise, float type with
+            range [0, 1]. Default: ``np.uint8``.
+        min_max (tuple[int]): min and max values for clamp.
+
+    Returns:
+        (Tensor or list): 3D ndarray of shape (H x W x C) OR 2D ndarray of
+        shape (H x W). The channel order is BGR.
+    """
+    if not (torch.is_tensor(tensor) or
+            (isinstance(tensor, list)
+             and all(torch.is_tensor(t) for t in tensor))):
+        raise TypeError(
+            f'tensor or list of tensors expected, got {type(tensor)}')
+
+    if torch.is_tensor(tensor):
+        tensor = [tensor]
+    result = []
+    for _tensor in tensor:
+        _tensor = _tensor.squeeze(0).float().detach().cpu().clamp_(*min_max)
+        _tensor = (_tensor - min_max[0]) / (min_max[1] - min_max[0])
+
+        n_dim = _tensor.dim()
+        if n_dim == 4:
+            img_np = make_grid(
+                _tensor, nrow=int(math.sqrt(_tensor.size(0))),
+                normalize=False).numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            if rgb2bgr:
+                img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 3:
+            img_np = _tensor.numpy()
+            img_np = img_np.transpose(1, 2, 0)
+            if img_np.shape[2] == 1:  # gray image
+                img_np = np.squeeze(img_np, axis=2)
+            elif img_np.shape[2] == 3:
+                if rgb2bgr:
+                    img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
+        elif n_dim == 2:
+            img_np = _tensor.numpy()
+        else:
+            raise TypeError('Only support 4D, 3D or 2D tensor. '
+                            f'But received with dimension: {n_dim}')
+        if out_type == np.uint8:
+            # Unlike MATLAB, numpy.unit8() WILL NOT round by default.
+            img_np = (img_np * 255.0).round()
+        img_np = img_np.astype(out_type)
+        result.append(img_np)
+    if len(result) == 1:
+        result = result[0]
+    return result
 
 
 class VX(BasicMachine):
@@ -292,3 +362,22 @@ class VX(BasicMachine):
 
         print("%s:PSNR:%.5f(%.5f),SSIM:%.5f(%.5f)"%(self.args.checkpoint,psnres.avg,psnresx.avg,ssimes.avg,ssimesx.avg))
         print("DONE.\n")
+
+    def test_single_image(self, image_path, input_size=256):
+        self.model.eval()
+        trans = transforms.Compose([
+                 transforms.Resize((input_size, input_size)),
+                 transforms.ToTensor()
+             ])
+        image = Image.open(image_path)
+        image_inp = trans(image)
+        image_inp = image_inp.unsqueeze(0)
+
+        image_inp = image_inp.to("cuda" if torch.cuda.is_available() else "cpu")
+
+        outputs = self.model(self.norm(image_inp))
+        imoutput,immask,imwatermark = outputs
+        imoutput = imoutput[0] if is_dic(imoutput) else imoutput
+        imfinal = self.denorm(imoutput*immask + self.norm(image_inp)*(1-immask))
+        return tensor2img(imfinal)
+
